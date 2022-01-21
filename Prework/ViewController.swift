32aa4cfc0c72f1266f7e8f086c5f1412def2e368
ThinPotato <bryce940@gmail.com>
2022-01-21 15:15:33 -0500

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Bryce Stoker on 1/21/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
